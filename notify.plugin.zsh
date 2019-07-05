4b9d7c12c31d6917e32c7109ad8ce3f42f5b917d
Federico Marzocchi <hello@mrz.io>
2019-07-05 10:56:32 +0200

    rename functions and other improvements

--- notify.plugin.zsh
+++ notify.plugin.zsh
@@ -1,26 +1,26 @@
 # vim: set nowrap filetype=zsh:
 # 
 # See README.md.
-#
-fpath=($fpath $(dirname $0:A))
 
+fpath=($fpath $(dirname $0:A))
 zstyle ':notify:*' resources-dir $(dirname $0:A)/resources
 zstyle ':notify:*' window-pid $WINDOWID
 
 test -z "$_ZSH_NOTIFY_ROOT_PPID" && export _ZSH_NOTIFY_ROOT_PPID="$PPID"
 zstyle ':notify:*' parent-pid $_ZSH_NOTIFY_ROOT_PPID
 
-# Notify an error with no regard to the time elapsed (but always only
+# Notify of an error with no regard to the time elapsed (but always only
 # when the terminal is in background).
 function notify-error {
     local time_elapsed 
     time_elapsed=$1
 
-    terminal-is-active || notify error "$time_elapsed" < /dev/stdin &!
+    terminal-is-active || zsh-notify error "$time_elapsed" < /dev/stdin &!
 }
 
-# Notify of successful command termination, but only if it took at least
-# 30 seconds (and if the terminal is in background).
+# Notify of successful command termination, but only if it took more than
+# the timeout set with command-complete-timeout, and only if the terminal
+# is in background).
 function notify-success() {
     local time_elapsed command_complete_timeout
 
@@ -30,7 +30,7 @@ function notify-success() {
         || command_complete_timeout=30
 
     if (( $time_elapsed > $command_complete_timeout )); then
-        terminal-is-active || notify success "$time_elapsed" < /dev/stdin &!
+        terminal-is-active || zsh-notify success "$time_elapsed" < /dev/stdin &!
     fi
 }
 
@@ -38,27 +38,33 @@ function notify-success() {
 function notify-command-complete() {
     last_status=$?
 
-    local now time_elapsed
+    local now time_elapsed error_log
 
-    if [[ -n $start_time ]]; then
-      now=`date "+%s"`
-      ((time_elapsed = $now - $start_time ))
-      if [[ $last_status -gt "0" ]]; then
-          notify-error "$time_elapsed" <<< $last_command
-      elif [[ -n $start_time ]]; then
-          notify-success "$time_elapsed" <<< $last_command
+    zstyle -s ':notify:' error-log error_log \
+      || error_log=/dev/stderr
+
+    (
+      if [[ -n $start_time ]]; then
+        now=`date "+%s"`
+        (( time_elapsed = $now - $start_time ))
+        if [[ $last_status -gt "0" ]]; then
+            notify-error "$time_elapsed" <<< $last_command
+        elif [[ -n $start_time ]]; then
+            notify-success "$time_elapsed" <<< $last_command
+        fi
       fi
-    fi
+    ) | sed 's/^/zsh-notify: /' 1> "$error_log" 2>&1
+
     unset last_command last_status start_time
 }
 
 function store-command-stats() {
-    last_command=$1
+    last_command="$1"
     start_time=`date "+%s"`
 }
 
 autoload add-zsh-hook
-autoload -U notify
+autoload -U zsh-notify
 autoload -U terminal-is-active
 add-zsh-hook preexec store-command-stats
 add-zsh-hook precmd notify-command-complete
